a45d6d684d53d21bd10660de53a5bcdb753977b3
AnghelloVera132 <143229189+AnghelloVera132@users.noreply.github.com>
2023-11-28 23:51:18 -0500

    Program.cs

--- Laboratorio 14/Program.cs
+++ Laboratorio 14/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Laboratorio_14
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            EncuestasC encuesta = new EncuestasC();
+           
+            do
+            {
+                Console.Clear();
+                Console.Write("===============================\n" +
+                "Encuesta Covid-19\n" +
+                "===============================\n" +
+                "1: Realizar Encuesta\n2: Mostrar datos de la encuesta\n3: Mostrar resultados\n4: Mostrar personas por edad\n5: Salir\n" +
+                "===============================\n" +
+                "Ingrese una opción: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        EncuestasC.quiz();
+                        break;
+                    case 2:
+                        EncuestasC.Data();
+                        break;
+                    case 3:
+                        EncuestasC.Resultados();
+                        break;
+                    case 4:
+                        EncuestasC.EdadPersona();
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (option != 5);
         }
     }
 }
